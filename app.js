5f5378b6ef2f2924753fcfa8b0b05d86746ad547
Dana Christopherson <dana.christopherson51@gmail.com>
2019-06-06 11:30:56 -0700

    Added new layout/content data

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 var express = require("express");
 var app = express();
-var listener = app.listen(3000, function(){
-	console.log("listen on port " + listener.address().port);
-});
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+	{name: "Salmon Creek", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+	{name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+	{name: "Mountain Goat's Rest", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+	{name: "Salmon Creek", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+	{name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+	{name: "Mountain Goat's Rest", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+	{name: "Salmon Creek", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+	{name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+	{name: "Mountain Goat's Rest", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+];
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "Salmon Creek", image:"https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-		{name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-		{name: "Mountain Goat's Rest", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-	]
-
 	res.render("campgrounds", { campgrounds: campgrounds });
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.post("/campgrounds", function(req, res){
+	// get data from form and add to a campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image:image}
+	campgrounds.push(newCampground);
+	// redirect back to campgrounds page
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
+});
+
+app.listen(3000, process.env.IP, function(){
 	console.log("The YelpCamp Server has started!");
 });
